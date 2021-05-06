29eb8dacfa63c794321f0e8bad23bb6427408da2
Mark Dietzer <git@doridian.net>
2021-05-05 19:08:25 -0700

    MOre proto

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import * as tjs from 'teslajs';
+import { writeFile, readFile } from 'fs/promises';
 
-// const [,username, password, mfa] = process.argv;
-const authData = require('../config/auth-data.json');
+interface TeslaJSTokenData {
+    authToken: string;
+    refreshToken: string;
+}
 
+let authData: TeslaJSTokenData | undefined;
+
+async function saveAuthData() {
+    if (!authData) {
+        return;
+    }
+    authData = {
+        authToken: authData.authToken,
+        refreshToken: authData.refreshToken,
+    };
+    await writeFile('../config/auth-data.json', JSON.stringify(authData));
+}
+
+async function tryReLogin() {
+    authData = JSON.parse(await readFile('../config/auth-data.json', 'utf8')) as TeslaJSTokenData;
+    authData = await tjs.refreshTokenAsync(authData.refreshToken);
+    saveAuthData()
+}
+
+async function tryLogin(data: tjs.LoginOptions) {
+    authData = await tjs.loginAsync(data);
+}
 
 async function main() {
-    await tjs.loginAsync({
-        username,
-        password,
-        mfaPassCode: mfa,
-    });
+    try {
+        await tryReLogin();
+    } catch(e) {
+        console.error('Error on ReLogin', e);
+    }
 }
 
 main().catch(e => console.error(e));
